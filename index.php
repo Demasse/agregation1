506e783424506fc896c7b12a5096a4360f9f116c
Demasse <demassedimas2006@2006.com>
2024-10-12 12:24:16 +0200

    BTS

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+class Moteur {
+    public $puissance;
+
+    public function __construct($puissance) {
+        $this->puissance = $puissance;
+    }
+}
+
+class Voiture {
+    public $moteurs = [];
+
+    public function ajouterMoteur($moteur) {
+        $this->moteurs[] = $moteur;
+    }
+}
+
+// Création des moteurs
+$moteur1 = new Moteur(50);
+$moteur2 = new Moteur(10);
+
+// Agrégation : ajout des moteurs à la voiture
+$voiture = new Voiture();
+$voiture->ajouterMoteur($moteur1);
+$voiture->ajouterMoteur($moteur2);
+
+print_r($voiture);
+
